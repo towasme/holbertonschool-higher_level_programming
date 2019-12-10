9f4778946f77eaa9ad3cbe0ed1b2d5aadafae8d0
towasme <towasm@gmail.com>
2019-12-10 22:05:41 +0000

    removed one print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            y = ord(i) - 32
-            print("{}".format(chr(y)), end="")
-        else:
-            print("{}".format(i), end="")
+        j = ord(i)
+        if j >= 97 and j <= 122:
+            j = j - 32
+        print("{}".format(chr(j)), end="")
     print()
