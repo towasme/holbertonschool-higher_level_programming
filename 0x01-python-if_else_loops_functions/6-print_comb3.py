fcfffd9dcb72cd5a484384c5b221e1a80314fdcf
towasme <towasm@gmail.com>
2019-12-10 20:51:13 +0000

    ans

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@ for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print("89")
-        print("{}{}".format(i, j), end=", ")
+        else:
+            print("{}{}".format(i, j), end=", ")
