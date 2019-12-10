6feb96dded99ae36c315c3277cffe86368c92d9c
towasme <towasm@gmail.com>
2019-12-10 22:52:03 +0000

    ans

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = (number * -1) % 10
+        print("{}".format(last), end="")
+        return (last)
+    else:
+        last = number % 10
+        print("{}".format(last), end="")
+        return (last)
