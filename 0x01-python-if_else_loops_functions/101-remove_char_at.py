c03e6ad190afe5ade259595f7b10364bf9b4d9da
towasme <towasm@gmail.com>
2019-12-11 04:01:52 +0000

    added conditionals for negative numbers

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str[:]:
-        
-
-
-for x  in range (len(str)
-      if x != n:
-      y[x] = str[x]
+    if n >= 0:
+        new = str[:n]
+        new = new + str[n+1:]
+        return (new)
+    else:
+        return (str)
