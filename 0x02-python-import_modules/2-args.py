98eea1544c97edc33f4438234f09b477c58c64d1
towasme <towasm@gmail.com>
2019-12-12 19:37:15 +0000

    added import conditional

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-arg = len(argv) - 1
-if arg == 0:
-    print('0 arguments.')
-elif arg == 1:
-    print("{} argument:".format(arg))
-elif arg > 1:
-    print("{} arguments:".format(arg))
-for i in range(arg):
-    if arg != 0:
-        print("{}: {}".format(i + 1, argv[i + 1]))
+if __name__ == "__main__":
+    from sys import argv
+    arg = len(argv) - 1
+    if arg == 0:
+        print('0 arguments.')
+    elif arg == 1:
+        print("{} argument:".format(arg))
+    elif arg > 1:
+        print("{} arguments:".format(arg))
+    for i in range(arg):
+        if arg != 0:
+            print("{}: {}".format(i + 1, argv[i + 1]))
