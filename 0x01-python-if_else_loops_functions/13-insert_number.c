bb5bdfc80c37a288b6dc91a35238f4c904d49f2c
towasme <towasm@gmail.com>
2019-12-12 04:43:22 +0000

    ans and corrected list name

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,10 +10,38 @@
  *Return: On success 1.
  */
 
-listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp = *head;
 	listint_t *new = NULL;
 
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
 	new->n = number;
 
+	while (temp)
+	{
+		if (temp->next == NULL)
+		{
+			new->next = temp;
+			temp->next = new;
+			return (new);
+		}
+		if (number < temp->next->n)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		if (head == NULL)
+		{
+			new->next = temp;
+			*head = new;
+			return (new);
+		}
+		temp = temp->next;
+	}
+	return (new);
+}
