47ae94dec9fbac4c2697dcd3841bce72a824356f
towasme <towasm@gmail.com>
2019-12-17 18:41:46 +0000

    fixed str use

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    str = "{}"
     for i in range(len(my_list)):
-        print(str.format(my_list[i]))
+        print("{:d}".format(my_list[i]))
