50b57134efee668a1f4b13159a93af16cd9e3584
towasme <towasm@gmail.com>
2019-12-17 18:44:02 +0000

    removed quotation marks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    str = "{}"
     if idx < 0:
         return ("None")
     elif idx > len(my_list):
         return ("None")
     else:
-        return my_list[idx]
+        return (my_list[idx])
