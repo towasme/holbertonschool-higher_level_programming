aeef46987453b6456cb963af1f5c4c919bb782f5
towasme <towasm@gmail.com>
2019-12-17 23:01:56 +0000

    ans 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        cont = 0
+        cont = my_list[0]
         for i in range(len(my_list)):
             if cont < my_list[i]:
                 cont = my_list[i]
