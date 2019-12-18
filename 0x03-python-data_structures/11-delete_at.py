2406889b3dfeb7aad991074081ce2f276d10ae0a
towasme <towasm@gmail.com>
2019-12-18 00:02:59 +0000

    ans 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        for i in range(len(my_list)):
+            if i == idx:
+                if idx > 0 or idx <= len(my_list):
+                    del my_list[i]
+                else:
+                    continue
+        return (my_list)
+    return ("None")
