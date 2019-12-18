9455f170f9ab9f32643178609624e93a0fb7baed
towasme <towasm@gmail.com>
2019-12-18 21:58:04 +0000

    ans 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *is_palindrome - prints a list length
+ *@head: the first position
+ *Return: On success 1.
+*/
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp = *head;
+	int i = 0, cont = 0;
+	int num_arr[1025];
+
+	if (!head)
+	{
+		return (0);
+	}
+	while (temp)
+	{
+		num_arr[i] = temp->n;
+		temp = temp->next;
+		i++;
+	}
+	while (i >= 0)
+	{
+		if (num_arr[cont] != num_arr[i - 1])
+		{
+			return (0);
+		}
+		else
+		{
+			i--;
+			cont++;
+		}
+	}
+	return (1);
+}
