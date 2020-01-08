4951985bd0e0ab8bccb9ed4639dc47c79c2e7fd1
towasme <towasm@gmail.com>
2020-01-08 19:19:34 +0000

    ans 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+#    try:
+    for i in range(x):
+        if type(my_list[i]) == int:
+            print("{:d}".format(my_list[i]), end="")
+            n += 1
+        else:
+            continue
+    print()
+    return n
+#   except IndexError:
+    print()
+    return n
