93494f85f4f85130d5c42c33a50613ad2b738f0e
towasme <towasm@gmail.com>
2020-01-08 18:35:18 +0000

    ans 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            n += 1
+        print ()
+        return n
+    except IndexError:
+        print()
+        return n
