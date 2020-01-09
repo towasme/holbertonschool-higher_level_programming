b3243dec233ba6f3b947e14084bd332a0d541a1e
towasme <towasm@gmail.com>
2020-01-09 22:56:52 +0000

    created a class, added a methond with a private attribute and a public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """class Square with a private attribute called size"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        self.square = self.__size * self.__size
+        return self.square
