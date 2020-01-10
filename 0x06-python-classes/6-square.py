23e297d7b9cb2b9aa17bdd7cf8063bb0c86f1e1e
towasme <towasm@gmail.com>
2020-01-10 05:15:07 +0000

    added conditionals and indentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,6 +6,20 @@ class Square:
         self.position = position
 
     @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or \
+            len(value) != 2 or \
+            type(value[0]) != int or type(value[1]) != int or \
+                value[0] < 0 or value[1] < 0:
+            raise TypeError(
+                "position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @property
     def size(self):
         return self.__size
 
@@ -16,16 +30,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if (value[0] > 0 or value[1] > 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
         self.square = self.__size * self.__size
