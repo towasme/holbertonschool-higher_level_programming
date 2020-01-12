f859719e4c892e79991a88353672e469db5b4038
towasme <towasm@gmail.com>
2020-01-12 21:00:20 +0000

    added dividing function and typeerrors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,15 +9,17 @@ function that divides a number with all the elements in a matrix
 
 def matrix_divided(matrix, div):
     """
-    Returns the matrix divided by the number with a float or int type.
-
-    if the number provided is not a int or a float then raise an TypeError.
-
-    Each row must be the same size otherwise raise a TypeError.
-
-    The matrix to be divided must have integers or float numbers otherwise raise a TypeError.
-
-    If the number that divides is 0 the raise a ZeroDivisionError.
+    Description:
+        Returns the matrix divided by the number with a float or int type.
+        If the number provided is not a int or a float then raise an TypeError.
+        Each row must be the same size otherwise raise a TypeError.
+        The matrix to be divided must have integers or float numbers otherwise raise a TypeError.
+        If the number that divides is 0 the raise a ZeroDivisionError.
+    Parameters:
+        matrix: list of list containing float or integers.
+        div: number used to divide each numebr in the matrix.
+    return:
+        New list.
     """
 
     row_size = len(matrix[0])
@@ -28,32 +30,33 @@ def matrix_divided(matrix, div):
     #check if matrix is a list of lists
     if type(matrix) is not list:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for row in matrix:
-        if type(row) is not list:
+    for i in range(len(matrix)):
+        if type(matrix[i]) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     #check if matrix has integers or floats
     for row in matrix:
         for elem in row:
-            if ((type(elem) is not int) or (type(elem) is not float)):
+            if type(elem) != int and type(elem) != float:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    #check if matrix has the same size
-
-    for x in range(len(matrix)):
-        print("{}\n", row_size)
-        print("{}\n", len(matrix))
-        if row_size != len(matrix[x]):
+    #check if all rows in the list have the same size
+    for row in matrix:
+        if row_size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
 
-    #check if number is integeror float
-    if type(div) is not int or type(div) is not float:
+    #check if number div is not integer or float
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     #check if number is zero
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    #divide
+    mini_list = []
     for a in range(len(matrix)):
         for b in range(len(matrix[a])):
-            new_matrix[a][b] = matrix[a][b] / div
+            mini_list.append(round((matrix[a][b]/div), 2))
+        new_matrix.append(mini_list)
     return new_matrix
