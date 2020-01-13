2459625ac68b8ff38b2ff462ded048c2d0916428
towasme <towasm@gmail.com>
2020-01-13 00:43:04 +0000

    created a function that prints a complete name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""
+say_my_name module:
+function that prints the first and last name of a person
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Parameters:
+        first_name: string containing the first name.
+        last_name: string containing the last name.
+    return:
+        print whole name
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    #print name and last name
+    print("My name is {} {}".format(first_name, last_name))
