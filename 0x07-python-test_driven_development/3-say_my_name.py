22177e781baf2eca8a0c02894fc8408f7ab9eecf
towasme <towasm@gmail.com>
2020-01-13 17:10:11 -0500

    added if name is empty

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,7 +15,7 @@ def say_my_name(first_name, last_name=""):
     return:
         print whole name
     """
-
+#    if
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
