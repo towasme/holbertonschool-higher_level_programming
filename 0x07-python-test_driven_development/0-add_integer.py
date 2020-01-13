a4d71a467f2ccee2c63a7710f6bbeb10ac35b9ae
towasme <towasm@gmail.com>
2020-01-13 04:18:10 +0000

    pep8 corrections

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
+
+
+"""
+add integers:
+Function that adds two integers or floats
+"""
+
+
 def add_integer(a, b=98):
-	if a == None:
-		raise TypeError("a must be a integer")
-	if b == None:
-		raise TypeError("b must be a integer")
-	if type(a) is not int and type(a) is not float:
-		raise TypeError("a must be an integer")
-	if type(b) is not int and type(a) is not float:
-		raise TypeError("b must be an integer")
-	return int(a + b)
+    """
+    Parameters:
+        a: integer
+        b: integer
+    Return:
+        the sum of the two numbers
+    """
+
+    if a is None:
+        raise TypeError("a must be a integer")
+    if b is None:
+        raise TypeError("b must be a integer")
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(a) is not float:
+        raise TypeError("b must be an integer")
+    if type(b) is tuple or type(b) is list or type(b) is str \
+            or type(b) is set:
+        raise TypeError("b must be an integer")
+    return int(a + b)
