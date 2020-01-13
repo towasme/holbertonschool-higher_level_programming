1b78277fd28261493cdc6d1deb4d571f63a41e53
towasme <towasm@gmail.com>
2020-01-13 14:47:41 -0500

    created a function that separates into strings everytime it encounters a . ? or :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+"""
+text_indentation module
+function that prints a text with 2 new lines after som specific characters
+"""
+
+
+def text_indentation(text):
+    """
+    Parameters:
+        text: the text the function will recive and then partitionate.
+    return:
+        print new text partitionate with new lines
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+#   #print the string
+    for j in range(len(text)):
+        if text[j] is '.' or text[j] is '?' or text[j] is ':':
+            print(text[j])
+            print()
+            j += 1
+        if text[j] == ' ' and text[j - 1] is '.' or text[j - 1] is '?' or text[j - 1] is ':':
+            continue
+        print(text[j], end="")
