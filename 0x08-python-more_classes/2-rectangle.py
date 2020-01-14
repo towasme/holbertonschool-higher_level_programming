16e8608a02c7620d41dc0d79ace96b50c254fb64
towasme <towasm@gmail.com>
2020-01-14 14:50:46 -0500

    class Rectangle with a function to perform

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    class Rectangle with two private attributes, width and height
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__height + self.__width) * 2
