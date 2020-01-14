3eb7aab6d57605c711c0893e28e52204545bb098
towasme <towasm@gmail.com>
2020-01-14 10:22:17 -0500

    added condition for init comments with # in first position in line

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -20,5 +20,5 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    #print name and last name
+#    #print name and last name
     print("My name is {:s} {:s}".format(first_name, last_name))
