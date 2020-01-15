03df5119477b7278af94ae7a96bd81b5c5baf378
towasme <towasm@gmail.com>
2020-01-15 05:39:48 +0000

    fixed arrea comparison

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -65,14 +65,14 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-    """
-    return the biggest rectangle from both comparison
-    """
+        """
+        return the biggest rectangle from both comparison
+        """
         if isinstance(rect_1, Rectangle) is False:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if isinstance(rect_2, Rectangle) is False:$
+        if isinstance(rect_2, Rectangle) is False:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1 >= rect_2:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
