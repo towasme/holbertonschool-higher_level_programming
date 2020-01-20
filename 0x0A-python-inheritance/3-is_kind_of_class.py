b1e02c98771f05caefcbbd7a716d4e7feebc24bb
towasme <towasm@gmail.com>
2020-01-20 23:54:17 +0000

    functions taht checks if its a instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Subclass Mylist that has a superclass named list
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function that returns true if the obj is an instance
+    """
+    return isinstance(obj, a_class)
