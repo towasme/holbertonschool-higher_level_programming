a309e5f6fca2130e633dcc1431165baa4eb9657a
towasme <towasm@gmail.com>
2020-01-21 01:15:42 +0000

    created an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+empty class
+"""
+
+
+class BaseGeometry:
+    """
+    class base geometry with a instance method area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
