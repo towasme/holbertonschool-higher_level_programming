30ddd1c4367a6951ce8016b816d42309aacb46c4
towasme <towasm@gmail.com>
2020-01-21 05:43:35 +0000

    order correction

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
+
 class MyList(list):
     """
     Subclass Mylist that has a superclass named list
     """
     def print_sorted(self):
-        self.list = list
         """
         function that sorts and prints a list.
         """
+        self.list = list
         print(sorted(self))
