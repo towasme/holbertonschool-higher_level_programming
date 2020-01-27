49bae29388d5745b0fab9f08b680cc6092d429e6
towasme <towasm@gmail.com>
2020-01-27 17:24:47 -0500

    corrected output in y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,7 +67,7 @@ class Rectangle(Base):
     def y(self, value):
         """y setter"""
         if type(value) is not int:
-            raise TypeError("y must be an integer y")
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
