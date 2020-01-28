7b8e60d59e0dc696d42ff119c237abdca3c7423a
towasme <towasm@gmail.com>
2020-01-28 04:03:30 +0000

    pep8 correction

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -13,52 +13,63 @@ class Testsquare(unittest.TestCase):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/square.py'])
-        self.assertEqual(result.total_errors, 0,
-                "Found code style errors (and warnings).")
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
 
     def test_getter(self):
         r1 = Square(5)
         self.assertEqual(r1.size, 5)
+
     def test_setter(self):
         r1 = Square(5)
         r1.size = 8
         self.assertEqual(r1.size, 8)
+
     def test_string(self):
         r1 = Square(3)
         with self.assertRaises(TypeError):
             r1.size = "Hi"
+
     def test_negative(self):
         r1 = Square(6)
         with self.assertRaises(ValueError):
             r1.size = -5
+
     def test_zero(self):
         r1 = Square(6)
         with self.assertRaises(ValueError):
             r1.size = 0
+
     def test_zero(self):
         r1 = Square(6)
         with self.assertRaises(TypeError):
             r1.size = 1.5
+
     def test_tupla(self):
         r1 = Square(7)
         with self.assertRaises(TypeError):
             r1.size = (2, 8)
+
     def test_empty(self):
         r1 = Square(7)
         with self.assertRaises(TypeError):
             r1.size = ""
+
     def test_none(self):
         r1 = Square(5)
         with self.assertRaises(TypeError):
             r1.size = None
+
     def test_list(self):
         r1 = Square(4)
         with self.assertRaises(TypeError):
             r1.size = [4, 7]
+
     def test_dict(self):
         r1 = Square(5)
         with self.assertRaises(TypeError):
             r1.size = {"hi": 5, "world": 8}
+
     def test_width(self):
         r1 = Square(5)
         r1.size = 6
