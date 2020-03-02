8e27370ae6fdbea4e5eabbc9e7d90ecb829a39c3
towasme <towasm@gmail.com>
2020-03-02 17:32:48 -0500

    delete script, update and sort

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+    name VARCHAR(256),
+    score INT);
+INSERT INTO second_table (id, name, score) VALUE ("1", "Jhon", "10");
+INSERT INTO second_table (id, name, score) VALUE ("2", "Alex", "3");
+INSERT INTO second_table (id, name, score) VALUE ("3", "Bob", "14");
+INSERT INTO second_table (id, name, score) VALUE ("4", "George", "8");
