7b333ad1673b6ac36ad8cdc187dff436f20ade17
towasme <towasm@gmail.com>
2020-03-02 17:54:45 -0500

    modified id and shcore values to int

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS second_table (id INT,
     name VARCHAR(256),
     score INT);
-INSERT INTO second_table (id, name, score) VALUE ("1", "Jhon", "10");
-INSERT INTO second_table (id, name, score) VALUE ("2", "Alex", "3");
-INSERT INTO second_table (id, name, score) VALUE ("3", "Bob", "14");
-INSERT INTO second_table (id, name, score) VALUE ("4", "George", "8");
+INSERT INTO second_table (id, name, score) VALUE (1, "Jhon", 10);
+INSERT INTO second_table (id, name, score) VALUE (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUE (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUE (4, "George", 8);
