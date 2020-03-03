05f9ab6faee4be8dc03392016caf33fb64f98fa5
towasme <towasm@gmail.com>
2020-03-02 21:40:40 -0500

    list all records and list of records with the same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,4 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT COUNT(score)
-FROM second_table
-WHERE 
-ORDER BY DESC;
+SELECT score, COUNT(*) FROM second_table
+GROUP BY score 
+ORDER BY number DESC;
