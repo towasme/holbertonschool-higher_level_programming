2e406a642abf0a05b718b64c4255d7dca0e8a785
towasme <towasm@gmail.com>
2020-03-04 00:04:22 -0500

    comments

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++
@@ -1,3 +0,0 @@
--- script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
-ALTER DATABASE hbtn_0c_0 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Import in hbtn_0c_0 database this table dump
+mysql -u root -p hbtn_0c_0 < home/casa/Descargas/temperatures.sql;
