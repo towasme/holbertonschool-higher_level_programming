504dc95dfddf0928a03248b2b0c60ef1438acc06
towasme <towasm@gmail.com>
2020-03-28 16:54:30 -0500

    added input restriction

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script that list all states starting with N from database
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """
+    main
+    """
+
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        passwd = sys.argv[2]
+        database = sys.argv[3]
+        argument = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost',
+                         user=username, passwd=passwd, db=database)
+
+    cur = db.cursor()
+    cur.execute('''SELECT * FROM states
+                 WHERE name = %s ORDER BY id ASC''', (argument,))
+
+    rows = cur.fetchall()
+
+    for state in rows:
+        print(state)
