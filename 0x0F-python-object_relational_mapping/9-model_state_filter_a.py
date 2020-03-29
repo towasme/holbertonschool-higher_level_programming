4af87b55f0176ea96f96a39dbdaacabc4f8c44ff
towasme <towasm@gmail.com>
2020-03-29 15:01:57 -0500

    search filtered with the letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script to list all State objects from database
+"""
+import sqlalchemy
+from model_state import State, Base
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+import re
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        if state.name.find('a'):
+            print("{}: {}".format(state.id, state.name))
+    session.close()
