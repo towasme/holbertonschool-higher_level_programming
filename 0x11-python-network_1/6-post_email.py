19963c04818f08333ff5055eaed80cc18bb5f78a
towasme <towasm@gmail.com>
2020-04-16 15:16:03 -0500

    excec permissions

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    payload = {'email': sys.argv[2]}
-    r = requests.post(url, data=payload)
+    data = {'email': sys.argv[2]}
+    r = requests.post(url, data)
     print(r.text)
