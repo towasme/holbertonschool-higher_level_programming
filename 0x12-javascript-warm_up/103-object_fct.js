6f10c5eb277ae4a5334860c33e370733706040f1
towasme <towasm@gmail.com>
2020-05-06 00:14:02 -0500

    incremnet

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value += 1;
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value += 1;
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
