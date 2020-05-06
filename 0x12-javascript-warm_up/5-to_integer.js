76646d4845b9fac08ac116a72dd671af95956d3f
towasme <towasm@gmail.com>
2020-05-05 20:34:18 -0500

    if its an int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
-const number = process.argv[2];
+const number = Number.isNaN(parseInt(process.argv[2]));
+
+if (number) {
+  console.log('Not a number');
+} else {
+  console.log('My number: %d', parseInt(process.argv[2]));
+}
