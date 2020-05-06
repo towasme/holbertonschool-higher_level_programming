bb894827f7bff4eedb737c3894dae7a56a9974bf
towasme <towasm@gmail.com>
2020-05-05 23:51:35 -0500

    conditionall for negative integers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv.length;
 
 if (args > 3) {
-  console.log(process.argv.sort((a,b)=>a-b).reverse()[1]);
+  console.log(process.argv.sort((a, b) => a - b).reverse()[1]);
 } else {
   console.log('0');
 }
