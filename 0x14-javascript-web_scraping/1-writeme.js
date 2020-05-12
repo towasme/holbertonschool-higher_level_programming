d4706d09f8aecbbf44e16c8076f5227a46e307bd
towasme <towasm@gmail.com>
2020-05-12 17:04:20 -0500

    removed utf8 added function

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const data = process.argv[3];
 // write on the file
-fs.writeFile(fileName, data, 'utf-8', (error) => {
+fs.writeFile(fileName, data, function (error, data) {
   if (error) {
     return console.log(error);
   }
-  console.log(data);
 });
