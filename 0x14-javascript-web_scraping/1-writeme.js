d1a771700c530ff9bd92d8698aa4ed65f0d32230
towasme <towasm@gmail.com>
2020-05-12 16:58:09 -0500

    removed data from callback

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const data = process.argv[3];
 // write on the file
-fs.writeFile(fileName, data, 'utf-8', (error, data) => {
+fs.writeFile(fileName, data, 'utf-8', (error) => {
   if (error) {
     return console.log(error);
   }
