20e1486e12178774382f1d7727ce896b927807b3
towasme <towasm@gmail.com>
2020-05-12 23:49:43 -0500

    semistrandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const request = require('request');
 const fileName = process.argv[3];
 const url = process.argv[2];
-//extract info
+// extract info
 request.get(url, function (error, body) {
-    if (error) {
-      console.log(error);
-    }
+  if (error) {
+    console.log(error);
+  }
 }).pipe(fs.createWriteStream(fileName, 'utf-8'));
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,30 +1,30 @@
 #!/usr/bin/node
 
-const request =require('request');
+const request = require('request');
 const name = '?completed=true';
 const dir = process.argv[2];
 const url = (dir + name);
 request.get(url, function (error, response, body) {
-    let data = {};
-    if (error) {
-      console.log(error);
-    }
-    data = (JSON.parse(body));
-    function obtain(data) {
-        let dict = {};
-        let i = 1; let j = 0;
-        for (let user in data) {
-            for(let valor in user) {
-                if (valor.userId === i) {
-                    j += 1;
-                    console.log('entra');
-                }
-                dict[i] = j;
-            }
-            i++;
-        }
-        console.log(dict);
-    }
-    obtain (data);
+  let data = {};
+  if (error) {
+    console.log(error);
   }
+  data = (JSON.parse(body));
+  function obtain (data) {
+    const dict = {};
+    let i = 1; let j = 0;
+    for (const user in data) {
+      for (const valor in user) {
+        if (valor.userId === i) {
+          j += 1;
+          console.log('entra');
+        }
+        dict[i] = j;
+      }
+      i++;
+    }
+    console.log(dict);
+  }
+  obtain(data);
+}
 );
