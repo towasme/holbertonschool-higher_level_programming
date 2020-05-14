871dc86fc0b760386b889d099615fe191389d9bd
towasme <towasm@gmail.com>
2020-05-13 21:00:18 -0500

    checks if user complete is true

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,20 +9,20 @@ request.get(url, function (error, response, body) {
   }
   data = (JSON.parse(body));
   function obtain (data) {
-    const dict = {};
-    let i = 0; let j = 0; let k = 1;
-    const user = [];
-    // const obj = Object;
-    //   for (user of data) {
-    //     if (Object.prototype.hasOwnProperty.call(obj, user.userId)) {
-    //       obj[user.userId] += 1;
-    //     } else {
-    //       obj[user.userId] = 1;
-    //     }
-    //   }
-    //   console.log(obj);
-    // }
-    data.forEach(element => {
+    let user;
+    const obj = Object;
+    for (user of data) {
+      if (user.completed === true) {
+        if (Object.prototype.hasOwnProperty.call(obj, user.userId)) {
+          obj[user.userId] += 1;
+        } else {
+          obj[user.userId] = 1;
+        }
+      }
+    }
+    console.log(obj);
+  }
+  /*    data.forEach(element => {
       if (element.completed === true) {
         user[i] = (element.userId);
         i++;
@@ -41,6 +41,6 @@ request.get(url, function (error, response, body) {
       }
     }
     console.log(dict);
-  }
+  } */
   obtain(data);
 });
