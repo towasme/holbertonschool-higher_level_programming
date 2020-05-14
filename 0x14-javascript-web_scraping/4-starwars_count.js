de73752b0e757f63de87ecae021dcd3a8bb4b741
towasme <towasm@gmail.com>
2020-05-13 21:37:48 -0500

    lookign for character in films

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
-const name = process.argv[2];
-const url = ('https://swapi-api.hbtn.io/api/films/' + name);
+const url = process.argv[2];
 
 request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  console.log(JSON.parse(body).title);
+  const result = (JSON.parse(body).results);
+  let cont = 0;
+  for (const movie of result) {
+    for (const chara of movie.characters) {
+      if (chara.includes('18')) {
+        cont += 1;
+      }
+    }
+  }
+  console.log(cont);
 });
