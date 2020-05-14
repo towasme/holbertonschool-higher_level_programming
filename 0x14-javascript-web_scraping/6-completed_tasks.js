519a18d332df83b6de57671f86adca8070bd0aaf
towasme <towasm@gmail.com>
2020-05-13 20:44:32 -0500

    fixed for when json  doesnt have completed true

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-// const name = '?completed=true';
-const dir = process.argv[2];
-const url = (dir);
+const url = process.argv[2];
 request.get(url, function (error, response, body) {
   let data = {};
   if (error) {
